7fec7f96162aebde8ba4fffaa9bdd8513e89fd24
Richard PIEL <r.piel@webandcow.com>
2021-03-05 09:07:39 +0100

    migrationsMigrate command

--- src/extension.ts
+++ src/extension.ts
@@ -35,81 +35,96 @@ export function activate(context: ExtensionContext) {
 
 			context.subscriptions.push(commands.registerCommand(`cakephp-bake.${cmdToExec.cmdName}`, async () => {
 
-					let cmd = `php ${workspacePath}/bin/cake.php bake ${cmdToExec.cmd}`;
+				let cmd = `php ${workspacePath}/bin/cake.php bake ${cmdToExec.cmd}`;
 
-					if (cmdToExec.options.selectClassType) {
-						const classType = await window.showQuickPick(classTypes, { placeHolder: 'Please select class type...' });
-						if (classType) {
-							cmd = `${cmd} ${classType.label}`;
-						}
+				if (cmdToExec.options.selectClassType) {
+					const classType = await window.showQuickPick(classTypes, { placeHolder: 'Please select class type...' });
+					if (classType) {
+						cmd = `${cmd} ${classType.label}`;
 					}
+				}
 
-					const model = await window.showInputBox({
-						placeHolder: `Please enter ${cmdToExec.humanName} name...`,
+				const model = await window.showInputBox({
+					placeHolder: `Please enter ${cmdToExec.humanName} name...`,
+				});
+				if (model) {
+					cmd = `${cmd} ${model}`;
+				}
+
+				if (cmdToExec.options.prefix) {
+					const prefix = await window.showInputBox({
+						placeHolder: "Please enter prefix name or leave empty...",
 					});
-					if (model) {
-						cmd = `${cmd} ${model}`;
+
+					if (prefix) {
+						cmd = `${cmd} --prefix ${prefix}`;
 					}
+				}
 
-					if (cmdToExec.options.prefix) {
-						const prefix = await window.showInputBox({
-							placeHolder: "Please enter prefix name or leave empty...",
-						});
-
-						if (prefix) {
-							cmd = `${cmd} --prefix ${prefix}`;
-						}
+				if (cmdToExec.options.plugin) {
+					const plugin = await window.showInputBox({
+						placeHolder: "Please enter plugin name or leave empty...",
+					});
+					if (plugin) {
+						cmd = `${cmd} --plugin ${plugin}`;
 					}
+				}
 
-					if (cmdToExec.options.plugin) {
-						const plugin = await window.showInputBox({
-							placeHolder: "Please enter plugin name or leave empty...",
-						});
-						if (plugin) {
-							cmd = `${cmd} --plugin ${plugin}`;
-						}
+				if (cmdToExec.options.forceOverwrite) {
+					const overwrite = await window.showInputBox({
+						placeHolder: "Overwrite? (y/N)",
+						value: "n",
+					});
+
+					if (overwrite === "y" || overwrite === "Y") {
+						cmd = `${cmd} --force`;
 					}
+				}
 
-					if (cmdToExec.options.forceOverwrite) {
-						const overwrite = await window.showInputBox({
-							placeHolder: "Overwrite? (y/N)",
-							value: "n",
-						});
+				cp.exec(
+					cmd,
+					{ timeout: timeout },
+					(err: string, stdout: string, stderr: string) => {
+						if (stderr) {
+							window.showErrorMessage(stripAnsi(stderr));
+						} else {
 
-						if (overwrite === "y" || overwrite === "Y") {
-							cmd = `${cmd} --force`;
-						}
-					}
+							window.showInformationMessage(
+								`${cmdToExec.humanName} file successfully created!`
+							);
 
-					cp.exec(
-						cmd,
-						{ timeout: timeout },
-						(err: string, stdout: string, stderr: string) => {
-							if (stderr) {
-								window.showErrorMessage(stripAnsi(stderr));
-							} else {
+							// Automatic file opener
+							if (cmdToExec.options.openFileCreated) {
+								const found = extractFilenameFromStdout(stdout);
 
-								window.showInformationMessage(
-									`${cmdToExec.humanName} file successfully created!`
-								);
-
-								// Automatic file opener
-								if (cmdToExec.options.openFileCreated) {
-									const found = extractFilenameFromStdout(stdout);
-
-									if (found !== null) {
-										var openPath = Uri.parse("file:///" + found);
-										workspace.openTextDocument(openPath).then((doc) => {
-											window.showTextDocument(doc);
-										});
-									}
+								if (found !== null) {
+									var openPath = Uri.parse("file:///" + found);
+									workspace.openTextDocument(openPath).then((doc) => {
+										window.showTextDocument(doc);
+									});
 								}
 							}
 						}
-					);
-				})
+					}
+				);
+			})
 			);
 		}
 	);
+	
+	context.subscriptions.push(commands.registerCommand('cakephp-bake.migrationsMigrate', async () => {
 
+		let cmd = `php ${workspacePath}/bin/cake.php migrations migrate`;
+
+		cp.exec(cmd, {"timeout": timeout} , (err: string, stdout: string, stderr: string) => {
+			if (stderr) {
+				window.showErrorMessage(stripAnsi(stderr));
+			} else {
+				window.showInformationMessage(
+					`Migration(s) successfully migrated!`
+				);
+			}
+		});
+
+	}));
 }
